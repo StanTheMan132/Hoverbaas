cab394ee8b94bdf5d26a4728822b85234ead1927
Stan-Cead <stan.hettinga@ceadgroup.com>
2025-06-05 15:57:04 +0200

    Hoek uitrekenen

--- Hoverbaas/Regelaar_19.ino
+++ Hoverbaas/Regelaar_19.ino
@@ -1,17 +1,19 @@
 void regelaar19() {
   const float Kp = 0.8, Ki = 0.0, Kd = 1.5;
 
-  static float fout_integratie = 0;
+  // static float fout_integratie = 0;
   static float vorige_fout = 0;
 
 
 
-  float fout = state.tof_right_back - state.tof_right_front;
-  fout_integratie += fout * dt;
-  float fout_afgeleide = (fout - vorige_fout) / dt;
+  float fout = (state.tof_right_back - state.tof_right_front) / 20;
+  // fout_integratie += fout * dt;
+  // float fout_afgeleide = (fout - vorige_fout) / dt;
   vorige_fout = fout;
 
-  float delta = Kp * fout + Ki * fout_integratie + Kd * fout_afgeleide;
+  // float delta = Kp * fout + Ki * fout_integratie + Kd * fout_afgeleide;
+  float delta = Kp * fout;
+
   delta = constrain(delta, -0.5, 0.5);
 
   float base = 0.0;

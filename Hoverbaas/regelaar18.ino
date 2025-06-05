405ef5e0f0b8c9de9900ded43a2416133fe6aec2
Stan-Cead <stan.hettinga@ceadgroup.com>
2025-06-05 15:48:14 +0200

    Make error static

--- Hoverbaas/regelaar18.ino
+++ Hoverbaas/regelaar18.ino
@@ -16,10 +16,20 @@ void regelaar18(){
   float x = 0.05;                         // afstand tot muur, beginwaarde (0.05m = 5cm)
   float ref = 0.20;                       // gewenste afstand tot muur (20 cm)
 
+  static float error = 0.0;
+  static float error_oud = 0.0;
+
+  unsigned long t_oud = 0;
+  unsigned long t_nu = 0;
+  float dt = 0.0;
 
 
 
 
+  t_nu = millis();
+  if (t_nu - t_oud >= 10) {  // elke 10 ms (100 Hz)
+    dt = (t_nu - t_oud) / 1000.0;
+    t_oud = t_nu;
 
     // === MEET X (afstand tot muur) ===
     if (!simulatie) {
@@ -28,11 +38,34 @@ void regelaar18(){
 
     // === PD-berekening ===
     error = ref - x;
-    float fout_afgeleide = (error - error_oud) / (dt / 1000);
+    float fout_afgeleide = (error - error_oud) / dt;
     error_oud = error;
 
     F = Kp * error + Kd * fout_afgeleide;
     F = constrain(F, Fmin, Fmax);
 
-    set_state.set_motor_middle_force = F; // kracht doorsturen
+    set_state.set_motor_middle_force = F;  // kracht doorsturen
+
+    // === Simulatie-uitvoer ===
+    if (simulatie) {
+      a = F / m;
+      v += a * dt;
+      x += v * dt;
+
+      Serial.print("x: "); Serial.print(x, 3);
+      Serial.print("  v: "); Serial.print(v, 3);
+      Serial.print("  F: "); Serial.println(F, 3);
+    }
+
+    /*
+    Serial.print("Afstand: ");
+    Serial.print(x * 100); // in cm
+    Serial.print(" cm\tF: ");
+    Serial.println(F, 3);
+    */
+  }
+  
+  
+
+
 }
